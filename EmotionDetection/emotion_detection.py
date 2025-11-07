8bce1056434a87f2be570e1731b133e859f40725
sergioenriquegm <sergioenriquegm@gmail.com>
2025-11-06 20:27:00 -0500

    Added error handling in emotion_detector

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,11 +6,25 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = {"raw_document": { "text": text_to_analyze }}
 
-    # Get the response
+    # Get the response 
     response = requests.post(url, json = input_json, headers = headers)
-    response_json = response.json()
-    emotions = response_json["emotionPredictions"][0]["emotion"] # Extract the emotions dictionary
-    dominant_emotion = max(emotions, key = emotions.get) # Extract the name of the dominant emotion
-    emotions["dominant_emotion"] = dominant_emotion # Adds the dominant emotion to the emotions dictionary
+
+    # In case of positive response code
+    if response.status_code == 200:
+        response_json = response.json()
+        emotions = response_json["emotionPredictions"][0]["emotion"] # Extract the emotions dictionary
+        dominant_emotion = max(emotions, key = emotions.get) # Extract the name of the dominant emotion
+        emotions["dominant_emotion"] = dominant_emotion # Adds the dominant emotion to the emotions dictionary
     
-    return emotions
+        return emotions
+    
+    # In case of error code 
+    elif response.status_code == 400:
+        return {
+        "anger" : None, 
+        "disgust" : None, 
+        "fear" : None, 
+        "joy" : None, 
+        "sadness" : None, 
+        "dominant_emotion" : None
+        }
